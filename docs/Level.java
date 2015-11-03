30840d02e9c19072f98a80f1cd955ce95b9b1d36
Christopher Roberson <croberson1988@gmail.com>
2015-11-03 17:54:29 -0600

    Level.java
    Class Name : Level

---
+++ docs/Level.java
@@ -0,0 +1,28 @@
+public class Level { 
+  private List<Block> blocks; 
+  private List<Decoration> decorations; 
+  private List<EnemySpawn> enemies; 
+  private Background bg; 
+  private Soundtrack music; 
+  
+  public List<Block> getBlocks(){
+    return blocks; 
+  }
+  
+  public List<Decoration> getDecoration(){
+    return decorations; 
+  }
+  
+  public List<EnemySpawn> getEnemies(){
+    return enemies; 
+  }
+  
+  public Background getBackground(){
+    return bg; 
+  }
+  
+  public Soundtrack getSoundtrack(){
+    return music; 
+  }
+  
+}
