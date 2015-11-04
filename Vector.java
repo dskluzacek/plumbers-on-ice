1dfd1ca8ff6dae893be20e18f0930188a672f3d2
jsabir <jamil.sabir3@gmail.com>
2015-11-03 18:06:06 -0600

    adding vector.java class

---
+++ Vector.java
@@ -0,0 +1,39 @@
+public class Vector {
+	private double x;
+	private double y;
+
+	public Vector(double a, double b) {
+		x = a;
+		y =b;
+	}
+
+	public double getX() {
+		return this.x;
+	}
+
+	public double getY() {
+		return this.y;
+	}
+
+	public void add(Vector r) {
+		this.x = this.x + r.getX();
+		this.y = this.y + r.getY();
+	}
+
+	public void subtract(Vector r) {
+		this.x = this.x - r.getX();
+		this.y = this.y - r.getY();
+	}
+
+	public void multiply(int i) {
+		this.x = this.x * i;
+		this.y = this.y * i;
+	}
+
+	public Vector sum(Vector r) {
+		double a = this.x + r.getX();
+		double b = this.y + r.getY();
+		return Vector(a, b);
+
+	}
+}
