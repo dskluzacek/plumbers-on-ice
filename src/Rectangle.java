de3d8c37b23a9c9789e9542cb6a6c2ca8db196f4
David Skluzacek <david.skluzacek@gmail.com>
2015-11-03 19:47:01 -0600

    Rectangle.intersects()

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,43 +1,52 @@
+
 public class Rectangle {
 	
-	int x;
-	int y;
-	int w;
-	int h;
+	private int x;
+	private int y;
+	private int w;
+	private int h;
 
 	public Rectangle(int x, int y, int w, int h) {
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
 	}
-
+ 
 	public int getX() {
-	return this.x;
+		return x;
 	}
+	
 	public int getY() {
-	return this.y;
+		return y;
 	}
+	
 	public int getW() {
-	return this.w;
+		return w;
 	}
+	
 	public int getH() {
-	return this.h;
+		return h;
 	}
+	
 	public void setX(int x) {
-	this.x = x;
+		this.x = x;
 	}
+	
 	public void setY(int y) {
-	this.y = y;
+		this.y = y;
 	}
+	
 	public void setW(int w) {
-	this.w = w;
+		this.w = w;
 	}
+	
 	public void setH(int h) {
-	this.h = h;
+		this.h = h;
 	}
 
-	public boolean intersects(Rectangle a) {
-	
+	public boolean intersects(Rectangle other) {
+		return ( this.x + this.w < other.x || other.x + other.w < this.x
+			    || this.y + this.h < other.y || other.y + other.h < this.y );
 	}
 }
