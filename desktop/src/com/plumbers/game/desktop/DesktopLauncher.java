165843d7bded3abff867d7fb070aac62c764d458
David Skluzacek <david.skluzacek@gmail.com>
2015-11-17 16:18:08 -0600

    Assets and libGDX project files

---
+++ desktop/src/com/plumbers/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.plumbers.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.plumbers.game.PlumbersOnIce;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 853;
+		config.height = 512;
+		
+		new LwjglApplication(new PlumbersOnIce(), config);
+	}
+}
