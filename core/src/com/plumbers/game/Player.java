84ead962fa7aaf10e1455a53537ef9dbfc2919ca
Christopher Roberson <croberson1988@gmail.com>
2015-11-17 18:54:24 -0600

    Update Player.java

--- core/src/com/plumbers/game/Player.java
+++ core/src/com/plumbers/game/Player.java
@@ -2,6 +2,14 @@ package com.plumbers.game;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.plumbers.game.Rectangle.Collision;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Player extends Character {
 
