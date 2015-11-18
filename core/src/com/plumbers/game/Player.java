b3119a62287898d20ca12da6e8e6af2a57962c13
Christopher Roberson <croberson1988@gmail.com>
2015-11-17 18:52:05 -0600

    Update Player.java

--- core/src/com/plumbers/game/Player.java
+++ core/src/com/plumbers/game/Player.java
@@ -7,6 +7,24 @@ public class Player extends Character {
 
 	public Player(String name) {
 		super(name);
+		textureAtlas =	new TextureAtlas(Gdx.files.internal(name+".atlas"), true);
+
+		TextureRegion[] walkFrames = new TextureRegion[] {
+				textureAtlas.findRegion(name+"-walk1"),
+				textureAtlas.findRegion(name+"-walk2"),
+				textureAtlas.findRegion(name+"-walk3") };
+		
+		TextureRegion[] idleFrames = new TextureRegion[] {
+				textureAtlas.findRegion(name+"-idle1"),
+				textureAtlas.findRegion(name+"-idle2"),
+				textureAtlas.findRegion(name+"-idle3") };
+
+		walkAnimation = new Animation(1/8f, walkFrames);
+		idleAnimation = new Animation(1/3f, idleFrames);
+		jumpAnimation = new Animation( 1, new TextureRegion[]{ textureAtlas.findRegion(name+"-jump") } );
+		knockbackAnimation = new Animation( 1, new TextureRegion[]{ textureAtlas.findRegion(name+"-knockback") } );
+		
+		this.movementAnim = new MovementAnimation(idleAnimation, walkAnimation, 5000, 300, jumpAnimation, null, null);
 	}
 
 	
