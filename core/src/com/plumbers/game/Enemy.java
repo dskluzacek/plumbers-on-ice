80a6111bddc71c11ceec21c67c90d97ab40411ba
Christopher Roberson <croberson1988@gmail.com>
2015-11-17 20:03:51 -0600

    Update Enemy.java

--- core/src/com/plumbers/game/Enemy.java
+++ core/src/com/plumbers/game/Enemy.java
@@ -1,14 +1,26 @@
 package com.plumbers.game;
 public class Enemy extends Character {
-
-	private String name;
-	private Rectangle hitbox;
-	private MovementAnimation movementAnim;
-	private State state;
-	private enum State {STANDING, RUNNING, JUMPING, FALLING, DYING};
 	
 	public Enemy(String name) {
-		this.name = name;
+		super(name);
+		textureAtlas =	new TextureAtlas(Gdx.files.internal(name+".atlas"), true);
+
+		TextureRegion[] walkFrames = new TextureRegion[] {
+				textureAtlas.findRegion(name+"-walk1"),
+				textureAtlas.findRegion(name+"-walk2"),
+				textureAtlas.findRegion(name+"-walk3") };
+		
+		TextureRegion[] idleFrames = new TextureRegion[] {
+				textureAtlas.findRegion(name+"-idle1"),
+				textureAtlas.findRegion(name+"-idle2"),
+				textureAtlas.findRegion(name+"-idle3") };
+
+		Animation walkAnimation = new Animation(1/8f, walkFrames);
+		Animation idleAnimation = new Animation(1/3f, idleFrames);
+		Animation jumpAnimation = new Animation( 1, new TextureRegion[]{ textureAtlas.findRegion(name+"-jump") } );
+		Animation knockbackAnimation = new Animation( 1, new TextureRegion[]{ textureAtlas.findRegion(name+"-knockback") } );
+		
+		this.movementAnim = new MovementAnimation(idleAnimation, walkAnimation, 5000, 300, jumpAnimation, null, null);
 	}
 	
 	public State getState() {
@@ -19,8 +31,20 @@ public class Enemy extends Character {
 		this.state = state;
 	}
 	
-	public void draw(Renderer rend) {
+	public void draw(Batch batch, float time) {
+		switch (state) {
+			case STANDING: movementAnim.setAction(Action.IDLE);
+			case RUNNING: movementAnim.setAction(Action.RUN);
+			case JUMPING: movementAnim.setAction(Action.JUMP);
+			case FALLING: movementAnim.setAction(Action.LAND);
+			case DYING: movementAnim.setAction(Action.KNOCKED_BACK);
+		}
+		movementAnim.setHorizontalSpeed( getVelocity().getX() );
 		
+		TextureRegion frame = movementAnim.getFrame(time);
+		Vector position = getPosition();
+		batch.draw(frame, position.getX(), position.getY(),
+				frame.getRegionWidth() * 2, frame.getRegionHeight() * 2);
 	}
 	
 	public void respondToCollision(Block b, Side s) {
