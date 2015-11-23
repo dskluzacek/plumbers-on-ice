92b0d82d610e2c68a15b89202e9e64cdbad2ccf3
David Skluzacek <david.skluzacek@gmail.com>
2015-11-22 21:32:18 -0600

    fix changed method name in RectangleTest

--- core/src/com/plumbers/game/RectangleTest.java
+++ core/src/com/plumbers/game/RectangleTest.java
@@ -6,59 +6,59 @@ public class RectangleTest {
 		Rectangle rect = new Rectangle(100, 100, 20, 30);
 		
 		Rectangle rect2 = new Rectangle(120, 130, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.TOP );
-		assert( rect.collisionInfo(rect2).getDistance() == 0 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.TOP );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 0 );
 		
 		rect2 = new Rectangle(90, 130, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.TOP );
-		assert( rect.collisionInfo(rect2).getDistance() == 0 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.TOP );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 0 );
 		
 		rect2 = new Rectangle(120, 90, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.LEFT );
-		assert( rect.collisionInfo(rect2).getDistance() == 0 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.LEFT );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 0 );
 		
 		rect2 = new Rectangle(90, 90, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.BOTTOM );
-		assert( rect.collisionInfo(rect2).getDistance() == 0 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.BOTTOM );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 0 );
 		
 		rect2 = new Rectangle(90, 100, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.RIGHT );
-		assert( rect.collisionInfo(rect2).getDistance() == 0 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.RIGHT );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 0 );
 
 		rect2 = new Rectangle(105, 129, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.TOP );
-		assert( rect.collisionInfo(rect2).getDistance() == 1 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.TOP );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 1 );
 		
 		rect2 = new Rectangle(115, 125, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.TOP );
-		assert( rect.collisionInfo(rect2).getDistance() == 5 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.TOP );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 5 );
 		
 		rect2 = new Rectangle(115, 124, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.LEFT );
-		assert( rect.collisionInfo(rect2).getDistance() == 5 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.LEFT );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 5 );
 		
 		rect2 = new Rectangle(115, 94, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.BOTTOM );
-		assert( rect.collisionInfo(rect2).getDistance() == 4 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.BOTTOM );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 4 );
 
 		rect2 = new Rectangle(115, 96, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.LEFT );
-		assert( rect.collisionInfo(rect2).getDistance() == 5 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.LEFT );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 5 );
 		
 		rect2 = new Rectangle(95, 97, 10, 10);
-		print( rect.collisionInfo(rect2) );
-		assert( rect.collisionInfo(rect2).getDirection() == Direction.RIGHT );
-		assert( rect.collisionInfo(rect2).getDistance() == 5 );
+		print( rect.staticCollisionInfo(rect2) );
+		assert( rect.staticCollisionInfo(rect2).getDirection() == Direction.RIGHT );
+		assert( rect.staticCollisionInfo(rect2).getDistance() == 5 );
 	}
 	
 	static void print(Rectangle.Collision info) {
