e459901d3ae11d02945604714508b4dee7aa02ea
David Skluzacek <david.skluzacek@gmail.com>
2015-11-28 04:59:24 -0600

    Implement enemies

--- core/src/com/plumbers/game/GameModel.java
+++ core/src/com/plumbers/game/GameModel.java
@@ -1,30 +1,30 @@
 package com.plumbers.game;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public final class GameModel {
-    private Player player1; 
-//  private Player p2;
+	private Player player1; 
+//  private Player p2; 
 	private Level currentLevel;
 	private List<Event> occuringEvents = new ArrayList<Event>(); 
-//  private List<EnemySpawn> enemies; 
-//  private Powerup powerups[]; 
+	private List<Enemy> enemies = new ArrayList<Enemy>(); 
 //  private List<Entity> entities;
-    private int gameTicks = 0;
+	private int gameTicks = 0;
 	
 	public static final float GRAVITY = 0.1f;
 
-    public GameModel(Level level, Player p1) {
-        currentLevel = level; 
-        player1 = p1; 
-    }
-    
-//  public GameModel(Level level, Player p1, Player p2) {
-//      player1 = p1; 
-//      player2 = p2; 
-//      currentLevel = level; 
-//  }
+	public GameModel(Level level, Player p1) {
+		currentLevel = level; 
+		player1 = p1; 
+	}
+	
+//	public GameModel(Level level, Player p1, Player p2) {
+//		player1 = p1; 
+//		player2 = p2; 
+//		currentLevel = level; 
+//	}
 
 	public List<Event> gameTick() {
 		++gameTicks;
@@ -38,6 +38,7 @@ public final class GameModel {
 		player1.fallingCheck( currentLevel.getBlocks() );
 		player1.collisionCheck( currentLevel.getBlocks() );
 		player1.hazardCollisionCheck( currentLevel.getHazards() );
+		player1.hazardCollisionCheck(enemies);
 		occuringEvents.addAll( player1.getEvents() );
 		occuringEvents.addAll(
 		      player1.coinCollectCheck( currentLevel.getCoins() ));
@@ -45,16 +46,33 @@ public final class GameModel {
 		if ( player1.fallingDeathCheck(512) ) {
 			occuringEvents.add( new DeathEvent(player1) );
 		}
+		
+		for (EnemySpawner spawner : currentLevel.getSpawners()) {
+			addNotNull(enemies, spawner.spawn( player1.getPosition().getX() ));
+		}
+		
+		for (Enemy enemy : enemies) {
+			enemy.simulate(gameTicks);
+			enemy.fallingCheck( currentLevel.getBlocks() );
+			enemy.collisionCheck( currentLevel.getBlocks() );
+		}
+		
 		return occuringEvents;
 	}
 
-    public Iterable<Drawable> getDrawables() {
-        List<Drawable> list = new ArrayList<Drawable>();
-        list.add(player1);
-        return list;
-    }
+	public Iterable<Drawable> getDrawables() {
+		List<Drawable> list = new ArrayList<Drawable>();
+		list.add(player1);
+		list.addAll(enemies);
+		return list;
+	}
 	
 	public void reset() {
+		for (EnemySpawner spawner : currentLevel.getSpawners()) {
+			spawner.reset();
+		}
+		
+		enemies.clear();
 		currentLevel.resetCoins();
 		gameTicks = 0;
 		player1.reset( currentLevel.getStartPosition() );
@@ -68,10 +86,9 @@ public final class GameModel {
 		return currentLevel.getWidthInTiles() * Block.SIZE;
 	}
 	
-	@SuppressWarnings("unused")
-	private void addEventNotNull(Event e) {
-		if (e != null) {
-			occuringEvents.add(e);
+	private static <T> void addNotNull(Collection<T> c, T obj) {
+		if (obj != null) {
+			c.add(obj);
 		}
 	}
 }
