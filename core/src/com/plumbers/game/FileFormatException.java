2191bd35c5a91c54802c3df89cf148d7809a3c60
David Skluzacek <david.skluzacek@gmail.com>
2015-11-27 18:50:30 -0600

    Add support for more level properties

---
+++ core/src/com/plumbers/game/FileFormatException.java
@@ -0,0 +1,22 @@
+package com.plumbers.game;
+
+public class FileFormatException extends Exception {
+	private static final long serialVersionUID = 6444554379261290003L;
+
+	public FileFormatException() {
+		super();
+	}
+
+	public FileFormatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FileFormatException(String message) {
+		super(message);
+	}
+
+	public FileFormatException(Throwable cause) {
+		super(cause);
+	}
+
+}
