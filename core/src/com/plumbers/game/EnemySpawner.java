e3d05352376d0c30f9aecd6d0e8628c2b3375b93
David Skluzacek <david.skluzacek@gmail.com>
2015-11-27 21:52:42 -0600

    Create EnemySpawner and start work on Enemy

---
+++ core/src/com/plumbers/game/EnemySpawner.java
@@ -0,0 +1,36 @@
+package com.plumbers.game;
+
+import com.plumbers.game.Enemy.Type;
+
+public class EnemySpawner {
+	private final int x, y;
+	private final Enemy.Type type;
+	private final int spawnDistance;
+	private boolean spawned = false;
+	private static final int DEFAULT_DISTANCE = 900;
+
+	public EnemySpawner(int x, int y, Type type) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+
+		spawnDistance = DEFAULT_DISTANCE; 
+	}
+
+	public EnemySpawner(int x, int y, Type type, int spawnDistance) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		this.spawnDistance = spawnDistance;
+	}
+
+	public Enemy spawn(float playerXPos) {
+		if (spawned || playerXPos + spawnDistance < x) {
+			return null;
+		}
+
+		return new Enemy(type,
+		                 x - type.relativeX - type.width / 2,
+		                 y - type.relativeY - type.height / 2);
+	}
+}
