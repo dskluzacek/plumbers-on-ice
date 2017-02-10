6b93ee684ade5a59fc4c289c8721b0f73481510b
David Skluzacek <david.skluzacek@gmail.com>
2017-02-10 15:37:21 -0600

    Code style changes and clean-up (MainMenu and PlumbersOnIceGame)

--- core/src/com/plumbers/game/ui/MainMenu.java
+++ core/src/com/plumbers/game/ui/MainMenu.java
@@ -14,9 +14,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.plumbers.game.GameCamera;
 
-public class MainMenu extends InputAdapter implements Screen {
+public final class MainMenu extends InputAdapter implements Screen
+{
     private Viewport viewport;
     private OrthographicCamera camera;
     private Sprite background;
@@ -27,165 +27,180 @@ public class MainMenu extends InputAdapter implements Screen {
     private ButtonGroup levelButtons;
     private Game theGame;
     private LevelsMenu levelMenu = new LevelsMenu();
-    
+
     private static String characterChoice = "hero";
-    
-//    public static final int GRASSY_LEVEL = 1,
-//            TROPICAL_LEVEL = 2,
-//            AUTUMN_LEVEL = 3,
-//            ICY_LEVEL = 4,
-//            CASTLE_LEVEL = 5;
+
     public static final String LEVEL_1_FILE = "grassy1.tmx",
                                LEVEL_2_FILE = "grassy2.tmx",
                                LEVEL_3_FILE = "winter1.tmx",
                                LEVEL_4_FILE = "fall1.tmx",
                                LEVEL_5_FILE = "castle1.tmx";
-    
-    public MainMenu(Game game) {
+
+    public MainMenu(Game game)
+    {
         theGame = game;
     }
-    
+
     @Override
-    public void show() {
-        if (batch != null) {
+    public void show()
+    {
+        if (batch != null)
+        {
             currentGroup = mainButtons;
             return;
         }
 
         batch = new SpriteBatch();
-        background = new Sprite( new Texture("menu.jpg") );
-        levelsBackground = new Sprite( new Texture("levels.jpg") );
+        background = new Sprite(new Texture("menu.jpg"));
+        levelsBackground = new Sprite(new Texture("levels.jpg"));
         background.setSize(1920, 1200);
         levelsBackground.setSize(1920, 1200);
-        
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1920, 1200);
         viewport = new FillViewport(1920, 1200, camera);
-        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),
+                true);
         viewport.apply(true);
         batch.setProjectionMatrix(camera.combined);
-        
+
         final int btnX = 560;
         final int btnWidth = 800;
         final int btnY = 212;
         final int btnHeight = 80;
         final int btnDiff = 27 + btnHeight;
-        
-        Button singlePlayerButton 
-            = new Button(btnX, btnY + 3*btnDiff, btnWidth, btnHeight,
-                    new Button.Listener() {
-                        public void action() {
-                            System.out.println("play");
-                            theGame.setScreen(levelMenu);
-                        }
-                    });
-        Button multiplayerButton
-            = new Button(btnX,  btnY + 2*btnDiff, btnWidth, btnHeight,
-                    new Button.Listener() {
-                        public void action() {
-                            
-                        }
-                    });
-        Button accountButton
-            = new Button(btnX, btnY + 1*btnDiff, btnWidth, btnHeight,
-                    new Button.Listener() {
-                        public void action() {
-                            System.out.println("accounts");
-                        }
-                    });
-        Button chooseCharButton
-            = new Button( btnX,  btnY, btnWidth, btnHeight,
-                    new Button.Listener() {
-                        public void action() {
-                            
-                        }
-                    });
-        
+
+        Button singlePlayerButton = new Button(btnX, btnY + 3 * btnDiff, btnWidth, btnHeight,
+                new Button.Listener()
+                {
+                    public void action()
+                    {
+                        System.out.println("play");
+                        theGame.setScreen(levelMenu);
+                    }
+                });
+        Button multiplayerButton = new Button(btnX, btnY + 2 * btnDiff, btnWidth, btnHeight,
+                new Button.Listener()
+                {
+                    public void action()
+                    {
+                    }
+                });
+        Button accountButton = new Button(btnX, btnY + 1 * btnDiff, btnWidth, btnHeight,
+                new Button.Listener()
+                {
+                    public void action()
+                    {
+                        System.out.println("accounts");
+                    }
+                });
+        Button chooseCharButton = new Button(btnX, btnY, btnWidth, btnHeight,
+                new Button.Listener()
+                {
+                    public void action()
+                    {
+                    }
+                });
+
         mainButtons = new ButtonGroup(new Button[] { singlePlayerButton,
                 multiplayerButton, accountButton, chooseCharButton });
-        
+
         currentGroup = mainButtons;
         Gdx.input.setInputProcessor(this);
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float delta)
+    {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
+
         batch.begin();
         background.draw(batch);
         batch.end();
-        
     }
-    
+
     @Override
     public boolean touchDown(int x, int y, int pointer, int btn)
     {
         Vector3 coords = new Vector3(x, y, 0);
         coords = camera.unproject(coords);
         currentGroup.touchDown(coords.x, coords.y);
-        
+
         return true;
     }
-    
+
     @Override
     public boolean touchUp(int x, int y, int pointer, int btn)
     {
         return true;
     }
-    
-    private class LevelsMenu extends ScreenAdapter {
-        
-        public LevelsMenu() {
+
+    private class LevelsMenu extends ScreenAdapter
+    {
+
+        public LevelsMenu()
+        {
             Button[] buttons = new Button[5];
-            
-            buttons[0] = new Button(510, 360, 340, 330,
-                    new Button.Listener() {
-                        public void action() {
-                            PlumbersOnIceGame.startSinglePlayer(LEVEL_1_FILE, characterChoice);
-                        }
-                    });
-            
-            buttons[1] = new Button(790, 360, 340, 330,
-                    new Button.Listener() {
-                        public void action() {
-                            PlumbersOnIceGame.startSinglePlayer(LEVEL_2_FILE, characterChoice);
-                        }
-                    });
-            
-            buttons[2] = new Button(1210, 360, 340, 330,
-                    new Button.Listener() {
-                        public void action() {
-                            PlumbersOnIceGame.startSinglePlayer(LEVEL_3_FILE, characterChoice);
-                        }
-                    });
-            buttons[3] = new Button(570, 130, 340, 330,
-                    new Button.Listener() {
-                        public void action() {
-                            PlumbersOnIceGame.startSinglePlayer(LEVEL_4_FILE, characterChoice);
-                        }
-                    });
-            buttons[4] = new Button(1010, 130, 340, 330,
-                new Button.Listener() {
-                    public void action() {
-                        PlumbersOnIceGame.startSinglePlayer(LEVEL_5_FILE, characterChoice);
-                    }
-                });
-        
+
+            buttons[0] = new Button(510, 360, 340, 330, new Button.Listener()
+            {
+                public void action()
+                {
+                    PlumbersOnIceGame.startSinglePlayer(LEVEL_1_FILE,
+                            characterChoice);
+                }
+            });
+
+            buttons[1] = new Button(790, 360, 340, 330, new Button.Listener()
+            {
+                public void action()
+                {
+                    PlumbersOnIceGame.startSinglePlayer(LEVEL_2_FILE,
+                            characterChoice);
+                }
+            });
+
+            buttons[2] = new Button(1210, 360, 340, 330, new Button.Listener()
+            {
+                public void action()
+                {
+                    PlumbersOnIceGame.startSinglePlayer(LEVEL_3_FILE,
+                            characterChoice);
+                }
+            });
+            buttons[3] = new Button(570, 130, 340, 330, new Button.Listener()
+            {
+                public void action()
+                {
+                    PlumbersOnIceGame.startSinglePlayer(LEVEL_4_FILE,
+                            characterChoice);
+                }
+            });
+            buttons[4] = new Button(1010, 130, 340, 330, new Button.Listener()
+            {
+                public void action()
+                {
+                    PlumbersOnIceGame.startSinglePlayer(LEVEL_5_FILE,
+                            characterChoice);
+                }
+            });
+
             levelButtons = new ButtonGroup(buttons);
         }
-        
+
         @Override
-        public void show() {
+        public void show()
+        {
             currentGroup = levelButtons;
         }
-        
+
         @Override
-        public void render(float delta) {
+        public void render(float delta)
+        {
             Gdx.gl.glClearColor(0, 0, 0, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            
+
             batch.begin();
             levelsBackground.draw(batch);
             batch.end();
@@ -193,31 +208,35 @@ public class MainMenu extends InputAdapter implements Screen {
     }
 
     @Override
-    public void pause() {
+    public void pause()
+    {
 
     }
 
     @Override
-    public void resume() {
+    public void resume()
+    {
         currentGroup = mainButtons;
         PlumbersOnIceGame.disposeGameView();
     }
 
     @Override
-    public void hide() {
+    public void hide()
+    {
 
     }
 
     @Override
-    public void dispose() {
+    public void dispose()
+    {
         batch.dispose();
         background.getTexture().dispose();
         levelsBackground.getTexture().dispose();
     }
 
     @Override
-    public void resize(int width, int height) {
-        
+    public void resize(int width, int height)
+    {
+
     }
-    
 }
--- core/src/com/plumbers/game/ui/PlumbersOnIceGame.java
+++ core/src/com/plumbers/game/ui/PlumbersOnIceGame.java
@@ -2,40 +2,45 @@ package com.plumbers.game.ui;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.plumbers.game.Controller;
 import com.plumbers.game.GameView;
 import com.plumbers.game.KeyboardController;
 import com.plumbers.game.TouchscreenController;
 
-public class PlumbersOnIceGame extends Game {
+public final class PlumbersOnIceGame extends Game
+{
     private static PlumbersOnIceGame instance;
     private MainMenu mainMenu;
     private GameView gameView;
     private Viewport viewport;
     private Controller controller;
-    
-    public static PlumbersOnIceGame createAndroidInstance(int displayWidth) {
-        if (instance != null)
+
+    public static PlumbersOnIceGame createAndroidInstance(int displayWidth)
+    {
+        if (instance != null) {
             throw new IllegalStateException();
+        }
         
         Viewport viewport = new ExtendViewport(1, 480);
         Controller controller = new TouchscreenController(displayWidth);
-        instance =  new PlumbersOnIceGame(viewport, controller);
+        instance = new PlumbersOnIceGame(viewport, controller);
         return instance;
     }
     
-    public static PlumbersOnIceGame createDefaultInstance() {
-        if (instance != null)
+    public static PlumbersOnIceGame createDefaultInstance()
+    {
+        if (instance != null) {
             throw new IllegalStateException();
+        }
         
         Viewport viewport = new ExtendViewport(1, 512);
         instance = new PlumbersOnIceGame( viewport, new KeyboardController() );
         return instance;
     }
     
-    public static void startSinglePlayer(String level, String character) {
+    public static void startSinglePlayer(String level, String character)
+    {
         instance.gameView = new GameView(level,
                                          character,
                                          instance.viewport,
@@ -46,23 +51,28 @@ public class PlumbersOnIceGame extends Game {
         instance.setScreen(instance.gameView);
     }
     
-    public static void returnToMenu() {
+    public static void returnToMenu()
+    {
         instance.setScreen(instance.mainMenu);
     }
     
-    private PlumbersOnIceGame(Viewport viewport, Controller controller) {
+    private PlumbersOnIceGame(Viewport viewport, Controller controller) 
+    {
         this.viewport = viewport;
         this.controller = controller;
     }
     
     @Override
-    public void create() {
+    public void create()
+    {
         mainMenu = new MainMenu(this);
         setScreen(mainMenu);
     }
 
-    public static void disposeGameView() {
-        if (instance.gameView != null) {
+    public static void disposeGameView()
+    {
+        if (instance.gameView != null)
+        {
             instance.gameView.dispose();
             instance.gameView = null;
         }
