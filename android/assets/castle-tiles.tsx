3955e116a746f5650ad987e10d6829e867d3938e
David Skluzacek <david.skluzacek@gmail.com>
2017-03-23 16:46:48 -0500

    Updated/in-progress tilesets and levels

--- android/assets/castle-tiles.tsx
+++ android/assets/castle-tiles.tsx
@@ -1,10 +1,45 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="castle-tiles" tilewidth="16" tileheight="16" spacing="2" margin="1">
  <image source="castle-tiles.png" width="198" height="198"/>
+ <tile id="1">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
  <tile id="7">
   <properties>
    <property name="height" value="12"/>
    <property name="relativeY" value="4"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="9">
@@ -15,6 +50,11 @@
  <tile id="10">
   <properties>
    <property name="waterfall1" value="1"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="12">
@@ -42,9 +82,19 @@
    <property name="height" value="12"/>
   </properties>
  </tile>
+ <tile id="17">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
  <tile id="18">
   <properties>
    <property name="height" value="13"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="20">
@@ -57,6 +107,26 @@
    <property name="waterfall1" value="3"/>
   </properties>
  </tile>
+ <tile id="22">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
  <tile id="31">
   <properties>
    <property name="waterfall1" value="4"/>
@@ -65,6 +135,21 @@
  <tile id="32">
   <properties>
    <property name="waterfall1" value="5"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="42">
@@ -77,6 +162,11 @@
    <property name="waterfall1" value="7"/>
   </properties>
  </tile>
+ <tile id="44">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
  <tile id="53">
   <properties>
    <property name="waterfall1" value="8"/>
@@ -85,6 +175,26 @@
  <tile id="54">
   <properties>
    <property name="waterfall1" value="9"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="73">
@@ -100,6 +210,46 @@
  <tile id="76">
   <properties>
    <property name="waterfall2" value="1"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="86">
@@ -144,7 +294,7 @@
  </tile>
  <tile id="94">
   <properties>
-   <property name="height" value="12"/>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="97">
@@ -155,6 +305,36 @@
  <tile id="98">
   <properties>
    <property name="waterfall2" value="5"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="decorative" value="true"/>
   </properties>
  </tile>
  <tile id="105">
---
+++ android/assets/grassland-tiles.tsx
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Grassland tiles" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="72" columns="8">
+ <image source="grassland-tiles.png" width="144" height="162"/>
+ <tile id="32">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="special" value="coin"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="special" value="spike"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="special" value="springboard"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="decorative" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="relativeX" value="8"/>
+   <property name="width" value="8"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="width" value="8"/>
+  </properties>
+ </tile>
+</tileset>
